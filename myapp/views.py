26093729a92bede6d494f95885068094255d712c
Siyad Savad <siyadsavad313@gmail.com>
2024-08-12 14:53:27 +0530

    12

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Books, Publisher, User
+from django.db.models import Sum
 
 
 
 def index(request):
-  author =Author.objects.all().filter(joindate__year__gte=2013)
+  author = Books.objects.all().filter(author__popularity_score__gte = 7).aggregate(total_price = Sum('price'))
+  print(author)
   res = ''
-  for i in author:
-    res += f'<h3>{i}</h3>'
+  for i,j in author.items():
+    res += f'<h3>{i}: {j}</h3>'
   return HttpResponse(res)
