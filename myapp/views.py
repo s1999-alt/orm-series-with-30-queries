1fe60566d57f006ea884f6493a762f48ad7f77c1
Siyad Savad <siyadsavad313@gmail.com>
2024-08-12 14:20:03 +0530

    10

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from .models import Author, Books, Publisher, User
 
 
 def index(request):
-  author =[Author.objects.all().order_by('joindate').first(), Author.objects.all().order_by('joindate').last()]
+  author =Author.objects.all().order_by('-joindate').values_list('firstname','lastname','joindate').first()
   res = ''
   for i in author:
     res += f'<h3>{i}</h3>'
