92cbf2b45ddfa89552cb2816ac5212aeb970c7c6
Siyad Savad <siyadsavad313@gmail.com>
2024-08-12 14:22:10 +0530

    11

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from .models import Author, Books, Publisher, User
 
 
 def index(request):
-  author =Author.objects.all().order_by('-joindate').values_list('firstname','lastname','joindate').first()
+  author =Author.objects.all().filter(joindate__year__gte=2013)
   res = ''
   for i in author:
     res += f'<h3>{i}</h3>'
