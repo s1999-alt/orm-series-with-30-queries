24d3690910219fc1a99c46e100b813b90ea57075
Siyad Savad <siyadsavad313@gmail.com>
2024-08-12 14:18:06 +0530

    query - 9

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Books, Publisher, User
 
 
+
+def index(request):
+  author =[Author.objects.all().order_by('joindate').first(), Author.objects.all().order_by('joindate').last()]
+  res = ''
+  for i in author:
+    res += f'<h3>{i}</h3>'
+  return HttpResponse(res)
