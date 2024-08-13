a0a9051c75a037c3f83db4308c8ad60dd5a42903
Siyad Savad <siyadsavad313@gmail.com>
2024-08-13 14:01:35 +0530

    23

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Books, Publisher, User
-from django.db.models import Sum
+from django.db.models import Sum,Q
 
 
 
 def index(request):
-  author = Author.objects.all().filter(books__title__icontains='le')
+  author = Author.objects.filter(Q(firstname__istartswith='a') and (Q(popularity_score__gte=5) or Q(joindate__year__gt=2014)))
   print(author)
   res = ''
   for i in author:
