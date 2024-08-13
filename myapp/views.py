b165db7a6fd2ceeb86b481ff4cf12dd0880b1f8a
Siyad Savad <siyadsavad313@gmail.com>
2024-08-13 17:25:20 +0530

    30

--- myapp/views.py
+++ myapp/views.py
@@ -6,9 +6,14 @@ from django.db.models import Sum, Q, Min, Max, Avg
 
 
 def index(request):
-  author = Author.objects.all().aggregate(Avg('popularity_score'))
-  print(author)
+  oldest_author = Author.objects.all().aggregate(Min('joindate'))
+  newest_author = Author.objects.all().aggregate(Max('joindate'))
+  avg_pop_score = Author.objects.all().aggregate(Avg('popularity_score'))
+  sum_price = Books.objects.all().aggregate(Sum('price'))
+
+  ans29 = [oldest_author, newest_author, avg_pop_score, sum_price]
+  print(ans29)
   res = ''
-  for i,j in author.items():
-    res += f'<h3>{i}: {round(j,2)}</h3>'
+  for i in ans29:
+    res += f'<h3>{i}</h3>'
   return HttpResponse(res)
