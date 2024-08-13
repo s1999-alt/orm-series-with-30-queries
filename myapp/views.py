cced39af36ac3af692684f02998953266dec7343
Siyad Savad <siyadsavad313@gmail.com>
2024-08-13 17:38:21 +0530

    31

--- myapp/views.py
+++ myapp/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Books, Publisher, User
-from django.db.models import Sum, Q, Min, Max, Avg
+from django.db.models import Sum, Q, Min, Max
 
 
 
 def index(request):
-  oldest_author = Author.objects.all().aggregate(Min('joindate'))
-  newest_author = Author.objects.all().aggregate(Max('joindate'))
-  avg_pop_score = Author.objects.all().aggregate(Avg('popularity_score'))
-  sum_price = Books.objects.all().aggregate(Sum('price'))
-
-  ans29 = [oldest_author, newest_author, avg_pop_score, sum_price]
-  print(ans29)
+  author = Author.objects.all().filter(recommendedby__isnull=True)
+  print(author)
   res = ''
-  for i in ans29:
+  for i in author:
     res += f'<h3>{i}</h3>'
   return HttpResponse(res)
