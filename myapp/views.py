a4b793c8d5ff3dcb4d6a540c8f766c884df4bf4c
Siyad Savad <siyadsavad313@gmail.com>
2024-08-13 22:25:24 +0530

    35

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Books, Publisher, User
-from django.db.models import Sum, Q, Min, Max
+from django.db.models import Sum, Q, Min, Max, Count
 
 
 
 def index(request):
-  author = Author.objects.all().filter(books__title__icontains='ab').count()
+  author = Author.objects.annotate(f_count=Count('followers')).filter(f_count__gt=1)
   print(author)
   res = ''
   for i in author:
-    res += f'<h3>{i}</h3>'
+    res += f'<h3>{i.firstname} : {i.f_count}</h3>'
   return HttpResponse(res)
